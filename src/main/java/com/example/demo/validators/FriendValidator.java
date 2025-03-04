98aa5f836cb0d9ffa599764d868ce42c638dd50b
SergiuMihaiTaran <plampe01@gmail.com>
2025-03-05 00:02:59 +0200

    V1.0

--- src/main/java/com/example/demo/validators/FriendValidator.java
+++ src/main/java/com/example/demo/validators/FriendValidator.java
@@ -6,9 +6,8 @@ import com.example.demo.domain.FriendShip;
 public class FriendValidator implements Validator<FriendShip> {
     @Override
     public void validate(FriendShip entity) throws ValidationException {
-        //TODO: implement method validate
         if(entity.getId1()==entity.getId2())
-            throw new ValidationException("Utilizatorul nu este valid");
+            throw new ValidationException("The friendship is not valid");
     }
 
 }
